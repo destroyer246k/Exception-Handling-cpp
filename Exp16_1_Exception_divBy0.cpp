694305eb9b36fdd82470161b93f8289be05695a3
Patel Vishwas <149381035+destroyer246k@users.noreply.github.com>
2025-09-27 11:06:24 +0530

    Update Exp16_1_Exception_divBy0.cpp

--- Exp16_1_Exception_divBy0.cpp
+++ Exp16_1_Exception_divBy0.cpp
@@ -24,3 +24,21 @@ int main() {
 
     return 0;
 }
+/* 
+Outputs:
+
+Case 1:
+Enter Number 1: 10
+Enter Number 2: 2
+Answer is: 5
+
+Case 2:
+Enter Number 1: 7
+Enter Number 2: 0
+Cannot Divide by 0
+
+Case 3:
+Enter Number 1: -12
+Enter Number 2: 4
+Answer is: -3
+*/
